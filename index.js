8700d9228a6a06a4c0042ca835a2d6261aa87f6e
md-ataur <mdataur15@gmail.com>
2021-11-10 22:20:06 +0600

    review api added

--- index.js
+++ index.js
@@ -24,26 +24,57 @@ async function run() {
         const productsCollection = database.collection("products");
         const ordersCollection = database.collection("orders");
         const usersCollection = database.collection("users");
+        const reviewsCollection = database.collection("reviews");
 
-        // GET API to get products
+        // GET API to get all products
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
         });
 
-        // POST API to add order
+        // GET API to get single product by id
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST API to add user order
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
             res.json(result)
         });
 
+        // GET API to get user orders by email
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const products = await ordersCollection.find(query).toArray();
+            res.send(products);
+        });
+
+        // POST API to add review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(result);
+        });
+
+        // GET API to get customer reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
         // POST API to add user
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
-            res.send(result);
+            res.json(result);
         });
 
         // PUT API to update user
